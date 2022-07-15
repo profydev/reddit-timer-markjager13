6ac7d1cba7e2d4933bce3783f6d274e7e2cf8067
Mark Jager <markjager13@gmail.com>
2022-07-15 06:56:30 -0400

    Create app skeleton #01 (#1)
    * Install fonts Bitter and Montserrat
    
    * Install normalize.css to reset default browser styles
    
    * Install styled-components
    
    * Create styles folder, add fonts, styled-normalize, globalStyles
    
    * Add router with routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import GlobalStyles from './styles/globalStyles';
+import Landing from './components/Landing';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <Router>
+        <GlobalStyles />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/search" element={<Search />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
